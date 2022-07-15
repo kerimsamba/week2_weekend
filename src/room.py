2ec5ab79746efcb3db9bd927a6212d444a0efcc4
Kerim Sangrar <k_sangrar@hotmail.com>
2022-07-15 13:27:34 +0100

    2nd commit

--- src/room.py
+++ src/room.py
@@ -2,4 +2,16 @@ class Room:
     
     def __init__(self, name):
        self.name = name
+       self.guest_list = []
+       self.playlist = []
+
+    def add_guest(self, guest_name):
+        self.guest_list.append(guest_name)
+
+    def add_song_to_playlist(self, song_to_be_added):
+        self.playlist.append(song_to_be_added)
+
+    def check_out_guest(self, guest_name):
+        self.guest_list.remove(guest_name)
+
 
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,14 +1,33 @@
 import unittest
 
 from src.room import Room
+from src.song import Song
+from src.guest import Guest
 
 class TestRoom(unittest.TestCase):
     
     def setUp(self):
         self.room = Room("Karoake Room")
+        self.guest = Guest("Arthur Dent")
+        self.song = Song("Wonderwall")
         
     def test_room_exists(self):
         self.assertEqual("Karoake Room", self.room.name)
+
+    def test_guest_can_enter_room(self):
+        self.room.add_guest("Arthur Dent")
+        self.assertEqual("Arthur Dent", self.room.guest_list[0])
+
+    def test_room_has_song_on_playlist(self):
+        self.room.add_song_to_playlist("Wonderwall")
+        self.assertEqual("Wonderwall", self.room.playlist[0])
+
+    def test_guest_can_leave_room(self):
+        self.room.add_guest("Arthur Dent")
+        self.assertEqual("Arthur Dent", self.room.guest_list[0])
+        self.room.check_out_guest("Arthur Dent")
+        self.assertEqual(0, len(self.room.guest_list))
+
 
 
 
